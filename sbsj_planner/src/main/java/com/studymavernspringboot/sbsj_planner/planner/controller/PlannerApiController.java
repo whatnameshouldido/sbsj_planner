6d4eb925603dfca2754218ba34a7608a674faddf
whatnameshouldido <pobyjun@naver.com>
2024-08-27 17:29:34 +0900

    little bit coding
    little bit coding

--- sbsj_planner/src/main/java/com/studymavernspringboot/sbsj_planner/planner/controller/PlannerApiController.java
+++ sbsj_planner/src/main/java/com/studymavernspringboot/sbsj_planner/planner/controller/PlannerApiController.java
@@ -1,4 +1,26 @@
 package com.studymavernspringboot.sbsj_planner.planner.controller;
 
+
+import com.studymavernspringboot.sbsj_planner.planner.Dto.PlanDto;
+import com.studymavernspringboot.sbsj_planner.planner.Service.impl.PlanServiceImpl;
+import com.studymavernspringboot.sbsj_planner.planner.Service.impl.PlannerServiceImpl;
+import com.studymavernspringboot.sbsj_planner.planner.domain.Planner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class PlannerApiController {
+    @Autowired
+    private PlannerServiceImpl plannerService;
+
+    @Autowired
+    private PlanServiceImpl planService;
+
+    @PostMapping("/users/{userid}/planners")
+    public ResponseEntity<String> plannerInsert(@RequestBody PlanDto param, @PathVariable("userid") String userId) {
+        Planner planner = plannerService.insertPlanner(param.getPlanner());
+    }
+
 }
--- sbsj_planner/src/main/java/com/studymavernspringboot/sbsj_planner/planner/domain/Plan.java
+++ sbsj_planner/src/main/java/com/studymavernspringboot/sbsj_planner/planner/domain/Plan.java
@@ -1,4 +1,15 @@
 package com.studymavernspringboot.sbsj_planner.planner.domain;
 
+
+import lombok.*;
+
+@Entity
+@Table(name="planner_tbl")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@SequenceGenerator
 public class Plan {
 }
