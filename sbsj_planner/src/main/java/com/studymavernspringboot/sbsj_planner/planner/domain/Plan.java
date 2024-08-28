b23f05eb2acac9c7bf6bc2fe6062306ce8b56c23
whatnameshouldido <pobyjun@naver.com>
2024-08-28 17:27:23 +0900

    add choosing time and ppt, excel
    add choosing time and ppt, excel

--- sbsj_planner/src/main/java/com/studymavernspringboot/sbsj_planner/planner/domain/Plan.java
+++ sbsj_planner/src/main/java/com/studymavernspringboot/sbsj_planner/planner/domain/Plan.java
@@ -1,6 +1,7 @@
 package com.studymavernspringboot.sbsj_planner.planner.domain;
 
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -10,6 +11,28 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 @Builder
-@SequenceGenerator
+@SequenceGenerator(name = "PLAN_SEQ_GENERATOR", sequenceName = "PLAN_SEQ", initialValue = 1, allocationSize = 1)
 public class Plan {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PLAN_SEQ_GENERATOR")
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 100)
+    private String title;
+
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    @Column(name = "startDate", nullable = false)
+    private String startDate;
+
+    @Column(name = "endDate", nullable = false)
+    private String endDate;
+
+    @Column(name = "memo", length = 100)
+    private String memo;
+
+    @Column(name = "people", nullable = false)
+    private String people;
 }
